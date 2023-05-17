75967d6896f793151b72a34270c5c0bd75e9ddb5
pauloboliveira <pborges086@hotmail.com>
2023-05-17 08:17:54 -0300

    Build main screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import blurBg from './src/assets/luz.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/logo-spacetime.svg'
+
+const StyleStripes = styled(Stripes)
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if(!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-zinc-50">
-        Open App.js to start working on your app!
-      </Text>
-      <StatusBar style="light" translucent />
+    <ImageBackground source={blurBg} className="relative px-8 py-10 flex-1 items-center bg-gray-900" imageStyle={{position: 'absolute', left: '-100%'}}>
+      <StyleStripes className='absolute left-2'/>
+
+    <View className='flex-1 items-center justify-center gap-6'>
+      <NLWLogo />
+      <View className='space-y-2'>
+        <Text className='text-center font-title text-2xl leading-tight text-gray-50'>Sua cápsula do tempo</Text>
+        <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+      </View>
+
+      <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-3'>
+        <Text className='font-alt text-sm uppercase text-black'>CADASTRAR LEMBRANÇA</Text>
+      </TouchableOpacity>
     </View>
+
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Feito com 💜 no NLW da Rocketseat</Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   )
 }
